59650c971556a47e1e9582900dea3194b344ae8e
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-07-01 14:39:40 +0700

    finish api

---
+++ app/Http/Controllers/API/VideoController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Video;
+
+class VideoController extends Controller
+{
+    public function index()
+    {
+        $videos = Video::latest()->paginate(4);
+        return response()->json([
+            "response" => [
+                "status"    => 200,
+                "message"   => "List Data Video"
+            ],
+            "data" => $videos
+        ], 200);
+    }
+
+    /**
+     * VideoHomePage
+     *
+     * @return void
+     */
+    public function VideoHomePage()
+    {
+        $videos = Video::latest()->take(2)->get();
+        return response()->json([
+            "response" => [
+                "status"    => 200,
+                "message"   => "List Data Video Homepage"
+            ],
+            "data" => $videos
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\API\TagController;
+use App\Http\Controllers\API\EventController;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\PhotoController;
+use App\Http\Controllers\API\PostController;
+use App\Http\Controllers\API\SliderController;
+use App\Http\Controllers\API\VideoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +29,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/kelas', [AuthController::class, 'kelas']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+//posts
+Route::get('/post', [PostController::class, 'index']);
+Route::get('/post/{id?}', [PostController::class, 'show']);
+Route::get('/homepage/post', [PostController::class, 'PostHomePage']);
+
+//events
+Route::get('/event', [EventController::class, 'index']);
+Route::get('/event/{slug?}', [EventController::class, 'show']);
+Route::get('/homepage/event', [EventController::class, 'EventHomePage']);
+
+//slider
+Route::get('/slider', [SliderController::class, 'index']);
+
+//tags
+Route::get('/tag', [TagController::class, 'index']);
+Route::get('/tag/{slug?}', [TagController::class, 'show']);
+
+//category
+Route::get('/category', [CategoryController::class, 'index']);
+Route::get('/category/{slug?}', [CategoryController::class, 'show']);
+
+//photo
+Route::get('/photo', [PhotoController::class, 'index']);
+Route::get('/homepage/photo', [PhotoController::class, 'PhotoHomepage']);
+
+//video
+Route::get('/video', [VideoController::class, 'index']);
+Route::get('/homepage/video', [VideoController::class, 'VideoHomepage']);
