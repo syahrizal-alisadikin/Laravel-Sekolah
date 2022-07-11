f0704ef37567fa9395dedb1efbbee5e8f3f43c65
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-07-11 16:17:12 +0700

    update carbon

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -19,7 +19,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::latest()->paginate(3);
+        $posts = Post::latest()->paginate(1);
         return response()->json([
             "response" => [
                 "status"    => 200,
