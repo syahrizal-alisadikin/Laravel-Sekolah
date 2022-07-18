9c661b595e7c254e9e5b30adeb609566992831ae
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-07-18 17:04:29 +0700

    tes crontab

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('loginfo:key')->everyMinute();
+        // $schedule->command('loginfo:key')->everyMinute();
     }
 
     /**
